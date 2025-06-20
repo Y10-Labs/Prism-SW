cabd8bdcda41a38e91d2a44f11851ce5d12eefd3
Manohark25 <147080856+Manohark25@users.noreply.github.com>
2025-06-20 22:17:03 +0530

    Update component.cpp

--- Game_Logic/component.cpp
+++ Game_Logic/component.cpp
@@ -1,23 +1,47 @@
 #pragma once
+#include <string>
 #include<iostream>
-#include<string>
-#include"collision.h"
-#include"event.h"
-#include"Hitbox.h"
-class Player : public collision, public event, public Hitbox {
+#include<vector>
+class Component {
 public:
-AABB hitbox;
-AABB GetHitbox() const override {
+    virtual ~Component() = default;
+};
+
+class Collision : public virtual Component {
+public:
+    virtual void OnCollision(Component* other) = 0;
+};
+
+class Event : public virtual Component {
+public:
+    virtual void HandleEvents(const std::string& event) = 0;
+};
+
+struct AABB {
+    float minX, minY, maxX, maxY; //the boundaries of the box...
+};
+
+class Hitbox : public virtual Component {
+public:
+    virtual AABB GetHitbox() const = 0;
+    virtual void SetHitbox(const AABB& box) = 0;
+};
+
+class Player : public virtual Collision, public virtual Event, public virtual Hitbox {
+public:
+    AABB hitbox;
+
+    Player(const AABB& box) : hitbox(box) {}
+
+    void OnCollision(Component* other) override {}
+
+    void HandleEvents(const std::string& event) override {}
+
+    AABB GetHitbox() const override {
         return hitbox;
     }
 
-
-Player(const AABB& box) : hitbox(box) {}
-void OnCollision(Component* other) override {
-    }
-void HandleEvents(const std::string& event) override {
-    }
-void SetHitbox(const AABB& box) override {
+    void SetHitbox(const AABB& box) override {
         hitbox = box;
     }
 };
